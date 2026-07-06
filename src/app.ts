b413b6d60c0cb3a04a1fb3d2a04c19b397457272
Shamim Hassan <shamimhassanpust@gmail.com>
2026-07-06 20:09:32 +0600

    create auth routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { errorHandler } from './middleware/errorHandler'
+import authRoutes from './routes/authRoutes'
 
 const app = express()
 
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.json({ success: true, message: 'GearUp API is running!' })
 })
+
+app.use('/api/auth', authRoutes)
 
 app.use('*', (req, res) => {
   res.status(404).json({ success: false, message: 'Route not found', errorDetails: null })
